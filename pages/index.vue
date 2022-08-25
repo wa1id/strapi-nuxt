905113d117d64e4c227178bea5aefa6723c233fc
Wa1iD <walid.yachou.ext@arvesta.eu>
2022-08-25 15:03:03 +0200

    add login form

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold">Strapi Nuxt</h1>
-  {{ response }}
+  <div>
+    <h1 class="text-3xl font-bold">Strapi Nuxt</h1>
+
+    <LoginForm />
+  </div>
 </template>
-
-<script setup>
-const { find } = useStrapi4();
-
-const response = await find("users", { populate: "*" });
-</script>
---
+++ pages/user.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>USER</h1>
+  {{ response }}
+</template>
+
+<script setup>
+const { find } = useStrapi4();
+
+const response = await find("users/me", {
+  populate: ["cv", "populate", "cv.job_experience"],
+});
+</script>
