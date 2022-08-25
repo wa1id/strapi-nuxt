46a38b9f381c445b532e0133f27e987869c79745
Wa1iD <walid.yachou.ext@arvesta.eu>
2022-08-25 10:55:59 +0200

    connect to strapi with GET request

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Strapi Nuxt</h1>
+  {{ response }}
+</template>
+
+<script setup>
+const { find } = useStrapi4();
+
+const response = await find("users", { populate: "*" });
+</script>
