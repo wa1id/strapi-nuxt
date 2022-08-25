b77a92ee4157efd40aee3c4e27d35f3672ef365c
Wa1iD <walid.yachou.ext@arvesta.eu>
2022-08-25 15:27:38 +0200

    add register form

--- pages/register.vue
+++ pages/register.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold p-6">Strapi Nuxt</h1>
+    <h1 class="text-3xl font-bold p-6">Sign up</h1>
 
     <section>
       <div class="px-6 h-full text-gray-800">
@@ -9,6 +9,16 @@
         >
           <div class="xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
             <form>
+              <div class="mb-6">
+                <input
+                  v-model="username"
+                  type="username"
+                  id="username"
+                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Username"
+                />
+              </div>
+
               <!-- Email input -->
               <div class="mb-6">
                 <input
@@ -36,16 +46,8 @@
                   @click="onSubmit"
                   class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                 >
-                  Login
+                  Sign me up!
                 </button>
-                <p class="text-sm font-semibold mt-2 pt-1 mb-0">
-                  Don't have an account?
-                  <a
-                    href="/register"
-                    class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                    >Register</a
-                  >
-                </p>
               </div>
             </form>
           </div>
@@ -56,15 +58,20 @@
 </template>
 
 <script setup>
-const { login } = useStrapiAuth();
+const { register } = useStrapiAuth();
 
 const router = useRouter();
+const username = ref();
 const email = ref();
 const password = ref();
 
 const onSubmit = async () => {
   try {
-    await login({ identifier: email.value, password: password.value });
+    await register({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
     router.push("/user");
   } catch (e) {}
 };
